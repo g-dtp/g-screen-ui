c5489677861253e44be01d15e5238b67e119b92e
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-02-15 10:51:41 +0800

    loading IE 报错

--- packages/g-loading/g-loading.vue
+++ packages/g-loading/g-loading.vue
@@ -1,37 +1,36 @@
 <template lang="pug">
-  .g-loading__contain(v-if="loading" :id="'g-loading__'+uid")
-    component(:is="lType")
-    .g-loading__text(v-if="text") {{text}}
+	.g-loading__contain(v-if="loading" :id="'g-loading__'+uid")
+		component(:is="lType")
+		.g-loading__text(v-if="text") {{text}}
 </template>
 
 <script>
-import boun from './type/double-boun'
-import wave from './type/wave'
-import circles from './type/circle'
-export default {
-  components: {
-    boun,
-    wave,
-    circles
-  },
-  data () {
-    return {
-    }
-  },
-  props: ['type', 'loading', 'text'],
-  computed: {
-    lType () {
-      return this.type || 'boun'
-    }
-  },
-  beforeCreate() {
-    this.uid = new Date().getTime()
-  },
-  mounted () {
-    // let options = document.getElementById(`g-loading__${this.uid}`)
-    // console.log(options.parentNode)
-    // options.parentNode.style.position = 'relative'
-    this.$el.parentElement.style.position = 'relative'
-  }
-}
+	import boun from './type/double-boun'
+	import wave from './type/wave'
+	import circles from './type/circle'
+
+	export default {
+		components: {
+			boun,
+			wave,
+			circles
+		},
+		data() {
+			return {}
+		},
+		props: ['type', 'loading', 'text'],
+		computed: {
+			lType() {
+				return this.type || 'boun'
+			}
+		},
+		beforeCreate() {
+			this.uid = new Date().getTime()
+		},
+		mounted() {
+			if(this.$el.parentElement){
+				this.$el.parentElement.style.position = 'relative'
+			}
+		}
+	}
 </script>
